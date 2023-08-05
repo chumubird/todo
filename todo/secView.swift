9eee56ba790b508de8d9716ed5abc9945bdd2a93
churoo <churoo0418@gmail.com>
2023-08-06 03:45:51 +0900

    테이블뷰-셀 크기위치 정렬 조정

--- todo/secView.swift
+++ todo/secView.swift
@@ -8,9 +8,9 @@
 import UIKit
 
 class secView: UIViewController {
-        
+    
     var names : [String] = []  //<-model
-    var editTextField : String?
+  //  var editTextField : [String] = []
     //MVC view model controller
     
     @IBOutlet weak var tableView: UITableView!
@@ -19,13 +19,13 @@ class secView: UIViewController {
     
     ///텍스트필드에 작성후 버튼을 눌르면 테이블뷰 셀에 적용됨
     @IBAction func didTabButton(_ sender: UIButton) {
-    
+        
         names.append(nameTextField.text ?? "empty")//업데이트됬어
         print(names)
         tableView.reloadData()//업데이트됫으니까 화면에 보여줘봐
         nameTextField.text = ""//<--이렇게해주면 버튼누르고 나서 텍스트필드 내용을 클리어 해줌
         saveNames() // <----이 버튼을 눌렀을때 names배열에 텍스트필드 입력값이 들어가고 저장이 되어야 어플에서 저장이 반영됨
-      
+        
     }
     
     @IBOutlet weak var secImg: UIImageView!
@@ -39,33 +39,17 @@ class secView: UIViewController {
         tableView.dataSource = self
         loadNames() // <----화면이 표시될 때마다 names 배열을 불러옴
     }
-
+    
     func saveNames() {
         UserDefaults.standard.set(names, forKey: "names") //<-- 유저디폴트 = 데이터저장소중에 하나 영구적 저장이 가능하며 간단한 데이터값들을 저장하기에 좋은 방법
     }
-
+    
     func loadNames() {
         if let savedNames = UserDefaults.standard.array(forKey: "names") as? [String] {
             names = savedNames // 불러온 배열이 nil이 아니라면 (데이터가 존재한다면)
             tableView.reloadData() // names 배열에 불러온 배열을 대입하고 테이블 뷰를 다시 로드하여 데이터를 반영
         }
     }
-//    override func viewWillAppear(_ animated: Bool) {
-//           super.viewWillAppear(animated)
-//
-//    }
-//
-//       func saveNames() {
-//           UserDefaults.standard.set(names, forKey: "names") //<-- 유저디폴트 = 데이터저장소중에 하나 영구적 저장이 가능하며 간단한 데이터값들을 저장하기에 좋은 방법
-//       }
-//
-//       func loadNames() {
-//           if let savedNames = UserDefaults.standard.array(forKey: "names") as? [String] {
-//               names = savedNames // 불러온 배열이 nil이 아니라면 (데이터가 존재한다면)
-//               tableView.reloadData() // names 배열에 불러온 배열을 대입하고 테이블 뷰를 다시 로드하여 데이터를 반영
-//           }
-//       }
-   
 }
 
 extension secView /*여기 파일이 secView 이고 class secView라서*/: UITableViewDelegate, UITableViewDataSource {
@@ -79,14 +63,13 @@ extension secView /*여기 파일이 secView 이고 class secView라서*/: UITab
         let cell = tableView.dequeueReusableCell(withIdentifier: "myFirstCell", for: indexPath)//인덱스 패스
         cell.textLabel?.text = names[indexPath.row]
         
+        
         // indexPath.section // 인덱스 패스란몇번째 섹션에 몇번째 로우 라는 뜻을 갖음
         //  indexPath.row
         // 즉 바로 위 함수는 총섹션 곱하기 로우 만큼 호출 시킴 0번째 섹션에 0번째 로우 1=인덱스 0
         //아이덴티파이어가 등록됬단 가정하에 재사용해서 셀을 그려준다
         //  cell.textLabel?.text = "hello chul"
-        if indexPath.row == 0 , case let self.editTextField = editTextField {
-            cell.textLabel?.text =  names[indexPath.row]
-        }
+
         return cell
     } // cellForRowAt <--- 한줄에 어떤 셀을 넣을건지 ?  (리턴에 UITableViewCell를 상수 let cell) 선언한것 이 들어갈거다
     // 테이블 뷰에 셀을 넣어줘야해  그래서 특정 셀 "마이 퍼스트 셀'을 가져왔음
@@ -118,48 +101,46 @@ extension secView /*여기 파일이 secView 이고 class secView라서*/: UITab
         }
         return UISwipeActionsConfiguration(actions: [deleteAction])
     }
-    
-    
     func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
         
         let editAction = UIContextualAction(style: .normal, title: "edit") { (action, sourceView, completionHandler) in
             let alertForEdit = UIAlertController(title: "수정", message: "수정할 것을 적으세요", preferredStyle: .alert)
+            
             //알럿창에 텍스트필드 만들어보기
-            alertForEdit.addTextField{
+            alertForEdit.addTextField {
                 textField in textField.text = self.names[indexPath.row]
+                
                 //알럿창에 오케이버튼 기능
                 let alertOk = UIAlertAction(title: "OK", style: .default) { (textField) in
                     if let textField = alertForEdit.textFields?.first {
                         if let editTextField = textField.text {
                             //  입력한 내용을 가져와서 처리하는 로직을 추가해야함
-                            print("입력한 내용: \(editTextField)")
+                            print("입력한 내용: (editTextField)")
                             
-                            self.editTextField =  editTextField
+                            // 배열에 입력한 내용 추가
+                            self.names[indexPath.row] = editTextField
+                            
+                            // 테이블 뷰 새로 리로드 하기
                             tableView.reloadData()
-                            
+                            self.saveNames()
                         }
                     }
                 }
                 alertForEdit.addAction(alertOk)
-
+                
                 // 4. 취소 버튼 추가
-                        let alertCancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-                        alertForEdit.addAction(alertCancel)
-
-
-                self.present(alertForEdit, animated: true, completion: nil)
-             
-
-                    }
-                }
-        return UISwipeActionsConfiguration(actions: [editAction])
-
-
+                let alertCancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+                alertForEdit.addAction(alertCancel)
             }
+            
+            self.present(alertForEdit, animated: true, completion: nil)
         }
-
         
-        //         return UISwipeActionsConfiguration(actions: [UIContextualAction(style: .normal, title: "edit", handler: {_,_, _ in })])
-        //          }
-        
+        return UISwipeActionsConfiguration(actions: [editAction])
+    }
+    
+    
+    
+    
+}
 
