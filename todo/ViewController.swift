4367a8b6b49b0eaba9409bb31fa2f5323aae59ce
churoo <churoo0418@gmail.com>
2023-08-28 23:57:22 +0900

    urlSession을 이용해서 고양이 랜덤 이미지 넣기

--- todo/ViewController.swift
+++ todo/ViewController.swift
@@ -9,27 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
     
-//
-//
-//    func deletedNamesUpdated(_ names: [String]) {
-//        if let savedDeletedNames = UserDefaults.standard.array(forKey: "deletedNames") as? [String] {
-//            deletedNames = savedDeletedNames
-//        }
-//    }
-//
+    //
+    //
+    //    func deletedNamesUpdated(_ names: [String]) {
+    //        if let savedDeletedNames = UserDefaults.standard.array(forKey: "deletedNames") as? [String] {
+    //            deletedNames = savedDeletedNames
+    //        }
+    //    }
+    //
     
     
-
     
     
+    let animalImage = "https://spartacodingclub.kr/css/images/scc-og.jpg"
     var deletedNames: [String] = []
     
     @IBOutlet weak var imgView: UIImageView! //이미지뷰는 변수 처리
     
     @IBAction func button1(_ sender: UIButton) {
         print("가 즈 ㅇ ㅏ ")
-        
-        
     }
     
     @IBAction func button2(_ sender: UIButton) {
@@ -39,14 +37,67 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         imgView.image = UIImage(named: "meta.jpeg")
-        // loadNames() 여기서 정보를 로드할필요가 사실 없는거같아서 주석처리해봤는대 영향이없었다 딱히 필요없는듯
         
         
     }
-    //    func loadNames() {
-    //        if let savedNames = UserDefaults.standard.array(forKey: "names") as? [String] {
-    //            names = savedNames   // 이것도 마찬가지로 딱히 필요없는것
-    //        }
-    //    }
+    override func viewWillDisappear(_ animated: Bool) {
+        showRandomeURLImage()
+    }
+    
+    func showRandomeURLImage () {
+        let urlString = "https://api.thecatapi.com/v1/images/search"
+        
+        if let url = URL(string: urlString) {
+            let task = URLSession.shared.dataTask(with: url) { data, response, error in
+                if let error = error {
+                    print("Error \(error)")
+                    return
+                }
+                if let data = data {
+                    do{
+                        if let JSONArray = try JSONSerialization.jsonObject(with: data, options: [] ) as? [[String: Any]],
+                           let firstImage = JSONArray.first,
+                           let urlString = firstImage["url"] as? String,
+                           let imageUrl = URL(string: urlString),
+                           let imageData = try? Data(contentsOf: imageUrl),
+                           let image = UIImage(data: imageData) {
+                            
+                            DispatchQueue.main.async {
+                                
+                                self.imgView.image = image
+                            }
+                        }
+                    }
+                    catch {
+                        print("JSON Parsing Error: \(error)")
+                    }
+                }
+            }
+            task.resume()
+        }
+    }
+    
+    
+    
+    
+    /* url세션 관련 공부한거 시도해보기*/
+    func getUrlData(){
+        if let url = URL(string: animalImage) {//옵셔널 바인딩 닐이 아닐때
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { data, response, error in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                if let JSONdata = data {
+                    let JSONdataString = String(data: JSONdata, encoding: .utf8)
+                    //print(JSONdata, response!)
+                    print(JSONdataString!)
+                }
+            }
+            task.resume()
+        }
+        
+    }
 }
 
