0ec3870451d82540a6b03285192a645db9158adf
churoo <churoo0418@gmail.com>
2023-09-03 18:40:56 +0900

    model 들을 분리 시켰다!

--- todo/controller/ViewController/secView.swift
+++ todo/controller/ViewController/secView.swift
@@ -8,47 +8,30 @@
 import UIKit
 
 
-class secView: UIViewController {
+class secView: UIViewController  {
+  
     
-    
-    
-    
-    var todoForAlone : [String] = []  //<-model
-    var todoForTogether : [String] = []
-    var deletedTodo : [String] = []
     
     
     @IBOutlet weak var tableView: UITableView!
-    
     @IBOutlet weak var nameTextField: UITextField!
-    
-    
     @IBOutlet weak var showPicker: UITextField!
     
-    var pickerList : [String] = ["-------" ,"혼자 할 일" , "같이 할 일"]
     @objc func action() {
-           showPicker.resignFirstResponder() // 피커뷰 닫기
+        showPicker.resignFirstResponder() // 피커뷰 닫기
        }
     
     @IBAction func goToDoneButton(_ sender: Any) {performSegue(withIdentifier: "showDeleted", sender: self)
     }
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "showDeleted", let destinationVC = segue.destination as? doneSecView {
-            destinationVC.deletedTodo = deletedTodo
+        if segue.identifier == "showDeleted", var destinationVC = segue.destination as? ToDoModel {
+            ToDoModel.shareToDoModel.deletedTodo = ToDoModel.shareToDoModel.deletedTodo
             print("던 투두 리스트 페이지로 가볼까 !")
         }
     }
-    
-    
-    
-   
     @IBAction func didTabButton(_ sender: UIButton) {
-        
-        
-        
-        
         if showPicker.text == "혼자 할 일" {
-                    todoForAlone.append(nameTextField.text ?? "empty")
+            ToDoModel.shareToDoModel.todoForAlone.append(nameTextField.text ?? "empty")
             secImg.image = UIImage(named: "pikachu.jpg")
             func showImageForAWhile() {
                 if self.secImg.isHidden == false {
@@ -60,7 +43,7 @@ class secView: UIViewController {
             }
                 }
         else if showPicker.text == "같이 할 일" {
-            todoForTogether.append(nameTextField.text ?? "empty")
+            ToDoModel.shareToDoModel.todoForTogether.append(nameTextField.text ?? "empty")
             secImg.image = UIImage(named: "realswift.jpg")
 
             func showImageForAWhile() {
@@ -72,11 +55,10 @@ class secView: UIViewController {
                 }
             }
                 }
-        
         tableView.reloadData()
         nameTextField.text = ""//<--이렇게해주면 버튼누르고 나서 텍스트필드 내용을 클리어 해줌
-        saveTodoForAlone()
-        saveTodoForTogether()
+        UserDefaultModel.userDefaultFunc.saveTodoForAlone()
+        UserDefaultModel.userDefaultFunc.saveTodoForTogether()
         
     }
     
@@ -84,7 +66,7 @@ class secView: UIViewController {
     @IBOutlet weak var hiddenImg: UIImageView!
     
     
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -92,29 +74,22 @@ class secView: UIViewController {
         hiddenImg.isHidden = true
         tableView.delegate = self
         tableView.dataSource = self
-        loadTodoForAlone() // <----화면이 표시될 때마다 names 배열을 불러옴
-        loadTodoForTogether()
-        loadDeletedTodo()
-        print("던 투두 목록 : \(deletedTodo)")
-
-        createPickerView(tagNo: 1)
-        dismissPickerView()
+        UserDefaultModel.userDefaultFunc.loadTodoForAlone() // <----화면이 표시될 때마다 names 배열을 불러옴
+        UserDefaultModel.userDefaultFunc.loadTodoForTogether()
+        UserDefaultModel.userDefaultFunc.loadDeletedTodo()
+        print("던 투두 목록 : \(ToDoModel.shareToDoModel.deletedTodo)")
+        
+            createPickerView(tagNo: 1)//createPickerView(tagNo: 1)
+            dismissPickerView()//dismissPickerView()
     }
-
     override func viewWillAppear(_ animated: Bool) {
-        loadDeletedTodo()
+        UserDefaultModel.userDefaultFunc.loadDeletedTodo()
     }
     
     override func viewDidAppear(_ animated: Bool) {
-        print("던 투두 목록 : \(deletedTodo)")
-        loadDeletedTodo()
+        print("던 투두 목록 : \(ToDoModel.shareToDoModel.deletedTodo)")
+        UserDefaultModel.userDefaultFunc.loadDeletedTodo()
     }
-
-    
-    
-    
-    
-    
 //    func ssackDaGulGirlWa () {
 //        let defaults = UserDefaults.standard
 //        let allKeys = defaults.dictionaryRepresentation().keys
@@ -125,45 +100,10 @@ class secView: UIViewController {
 //            }
 //        }
 //    }
-    
-    
-    
-    func saveTodoForAlone() {
-        UserDefaults.standard.set(todoForAlone, forKey: "todoForAlone") //<-- 유저디폴트 = 데이터저장소중에 하나 영구적 저장이 가능하며 간단한 데이터값들을 저장하기에 좋은 방법
-    }
-    
-    func loadTodoForAlone() {
-        if let savedTodoForAlone = UserDefaults.standard.array(forKey: "todoForAlone") as? [String] {
-            todoForAlone = savedTodoForAlone // 불러온 배열이 nil이 아니라면 (데이터가 존재한다면)
-            //Thread.sleep(forTimeInterval: 2)
-            tableView.reloadData() // names 배열에 불러온 배열을 대입하고 테이블 뷰를 다시 로드하여 데이터를 반영
-        }
-    }
-    
-    func saveTodoForTogether() {
-        UserDefaults.standard.set(todoForTogether, forKey: "todoForTogether")
-    }
-    func loadTodoForTogether() {
-        if let savedTodoForTogether = UserDefaults.standard.array(forKey: "todoForTogether") as? [String] {
-            todoForTogether = savedTodoForTogether
-            //Thread.sleep(forTimeInterval: 2)
-            tableView.reloadData()
-        }
-    }
-    /*유저디폴트로 데이터 저장 통일을 위해서 이번에 새로 만듬*/
-    func saveDeletedTodo() {
-        UserDefaults.standard.set(deletedTodo, forKey: "deletedTodo")
-    }
-    func loadDeletedTodo() {
-        if let saveDeletedTodo = UserDefaults.standard.array(forKey: "deletedTodo") as? [String] {
-            deletedTodo = saveDeletedTodo
-            
-        }
-    }
+
 }
 
 extension secView /*여기 파일이 secView 이고 class secView라서*/: UITableViewDelegate, UITableViewDataSource {
-    
     func numberOfSections(in tableView: UITableView) -> Int {
         return 2       } // 섹션을 두개 만들어줌 : 혼자할일 / 같이할일 이렇게 두 카테고리로
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
@@ -174,30 +114,28 @@ extension secView /*여기 파일이 secView 이고 class secView라서*/: UITab
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         return 40.0 // 원하는 섹션 높이로 수정해주세요
     }
-    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if section == 0 {
-            return todoForAlone.count
+            return ToDoModel.shareToDoModel.todoForAlone.count
         }
         else {
-            return todoForTogether.count
+            return ToDoModel.shareToDoModel.todoForTogether.count
         }
     }     //numberOfRowsInSection <---- 하나의 섹션에 몇개의 로우를 만들거야 ? 즉 태이블 뷰가 몇줄이 나오는지는 이 함수에서 정함
-    
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "myFirstCell", for: indexPath)//인덱스 패스
         if indexPath.section == 0 { // 첫 번째 섹션
-                cell.textLabel?.text = todoForAlone[indexPath.row]
+            cell.textLabel?.text = ToDoModel.shareToDoModel.todoForAlone[indexPath.row]
             } else if indexPath.section == 1 { // 두 번째 섹션
-                cell.textLabel?.text = todoForTogether[indexPath.row]
+                cell.textLabel?.text = ToDoModel.shareToDoModel.todoForTogether[indexPath.row]
             }
-        print("\(todoForAlone) 혼자 할 일 리스트 현황")
-        print("\(todoForTogether) 같이 할 일 리스트 현황")
+        print("\(ToDoModel.shareToDoModel.todoForAlone) 혼자 할 일 리스트 현황")
+        print("\(ToDoModel.shareToDoModel.todoForTogether) 같이 할 일 리스트 현황")
 
         return cell
     }
-    
+
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if indexPath.row == 0 {
             tableView.deselectRow(at: indexPath, animated: true)
@@ -205,45 +143,45 @@ extension secView /*여기 파일이 secView 이고 class secView라서*/: UITab
             tableView.deselectRow(at: indexPath, animated: true)
         }
     }
-  
+
     func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
         let doneAction = UIContextualAction(style: .destructive, title: "DONE") { (action, sourceView, completionHandler) in
             if indexPath.section == 0 {
-                self.deletedTodo.append(self.todoForAlone[indexPath.row])
-                self.todoForAlone.remove(at: indexPath.row) // 데이터 배열에서 해당 인덱스의 데이터를 삭제
+                ToDoModel.shareToDoModel.deletedTodo.append(ToDoModel.shareToDoModel.todoForAlone[indexPath.row])
+                ToDoModel.shareToDoModel.todoForAlone.remove(at: indexPath.row) // 데이터 배열에서 해당 인덱스의 데이터를 삭제
                 tableView.reloadData() // 테이블 뷰를 리로드하여 삭제된 데이터를 반영해주기
-                self.saveTodoForAlone()
+                UserDefaultModel.userDefaultFunc.saveTodoForAlone()
                 //유저디폴트로 데이터 저장 전달하려고 기존 방식주석처리
                 //self.sendingData?.deletedNamesUpdated(self.deletedNames) // 델리게이트를 통해 삭제된 데이터 전달
                 completionHandler(true) // 삭제기능
-                print("\(self.deletedTodo) 던 투두 리스트 현황")
+                print("\(ToDoModel.shareToDoModel.deletedTodo) 던 투두 리스트 현황")
                 //print("유저디폴트에 저장된 던 투두 목록 : \(self.saveDeletedNames())")
             }
             else if indexPath.section == 1 {
-                self.deletedTodo.append(self.todoForTogether[indexPath.row])
-                self.todoForTogether.remove(at: indexPath.row)
+                ToDoModel.shareToDoModel.deletedTodo.append(ToDoModel.shareToDoModel.todoForTogether[indexPath.row])
+                ToDoModel.shareToDoModel.todoForTogether.remove(at: indexPath.row)
                 tableView.reloadData()
-                self.saveTodoForTogether()
+                UserDefaultModel.userDefaultFunc.saveTodoForTogether()
                 completionHandler(true)
-                print("\(self.deletedTodo) 던 투두 리스트 현황")
+                print("\(ToDoModel.shareToDoModel.deletedTodo) 던 투두 리스트 현황")
             }
         }
         doneAction.backgroundColor = .systemMint //배경색 넣어주기 스와이프 길이 조절해주려면 해야한다고함 이유는 솔직히 모름 뭔가 공식같은거같아서 그냥 공식같은거같은듯 ?
       
         let deleteAction = UIContextualAction(style: .destructive, title: "DELETE") { (action, sourceView, completionHandler) in
             if indexPath.section == 0 {
-                self.todoForAlone.remove(at: indexPath.row)
+                ToDoModel.shareToDoModel.todoForAlone.remove(at: indexPath.row)
                 tableView.reloadData()
-                self.saveTodoForAlone()
+                UserDefaultModel.userDefaultFunc.saveTodoForAlone()
                 completionHandler(true)
-                print("삭제후 남은 forAlone 리스트 목록:\(self.todoForAlone)") //<==== 오류가남;;
+                print("삭제후 남은 forAlone 리스트 목록:\(ToDoModel.shareToDoModel.todoForAlone)") //<==== 오류가남;;
             }
             else if indexPath.section == 1 {
-                self.todoForTogether.remove(at: indexPath.row)
+                ToDoModel.shareToDoModel.todoForTogether.remove(at: indexPath.row)
                 tableView.reloadData()
-                self.saveTodoForTogether()
+                UserDefaultModel.userDefaultFunc.saveTodoForTogether()
                 completionHandler(true)
-                print("삭제후 남은 forTogether 리스트 목록:\(self.todoForTogether)")
+                print("삭제후 남은 forTogether 리스트 목록:\(ToDoModel.shareToDoModel.todoForTogether)")
                 
             }
         }
@@ -259,7 +197,7 @@ extension secView /*여기 파일이 secView 이고 class secView라서*/: UITab
             
             //알럿창에 텍스트필드 만들어보기
             alertForEdit.addTextField {
-                textField in textField.text = indexPath.section == 0 ? self.todoForAlone[indexPath.row] : self.todoForTogether[indexPath.row]
+                textField in textField.text = indexPath.section == 0 ? ToDoModel.shareToDoModel.todoForAlone[indexPath.row] : ToDoModel.shareToDoModel.todoForTogether[indexPath.row]
                 
                 //알럿창에 오케이버튼 기능
                 let alertOk = UIAlertAction(title: "OK", style: .default) { (textField) in
@@ -269,14 +207,14 @@ extension secView /*여기 파일이 secView 이고 class secView라서*/: UITab
                             print("입력한 내용: (editTextField)")
                             if indexPath.section == 0 {
                                 // 배열에 입력한 내용 추가
-                                self.todoForAlone[indexPath.row] = editTextField
+                                ToDoModel.shareToDoModel.todoForAlone[indexPath.row] = editTextField
                             }
                             else if indexPath.section == 1 {
-                                self.todoForTogether[indexPath.row] = editTextField
+                                ToDoModel.shareToDoModel.todoForTogether[indexPath.row] = editTextField
                             }
                             // 테이블 뷰 새로 리로드 하기
-                            self.saveTodoForAlone()
-                            self.saveTodoForTogether()
+                            UserDefaultModel.userDefaultFunc.saveTodoForAlone()
+                            UserDefaultModel.userDefaultFunc.saveTodoForTogether()
                             tableView.reloadData()
                             
                             print("수정됨")
@@ -298,19 +236,26 @@ extension secView /*여기 파일이 secView 이고 class secView라서*/: UITab
 }
 
 
+
+
+
+
+
 extension secView : UIPickerViewDelegate, UIPickerViewDataSource {
-    
+
+
+
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return pickerList.count
+        return PickerModel.pickerModel.pickerList.count
     }
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return pickerList[row]
+        return PickerModel.pickerModel.pickerList[row]
     }
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        showPicker.text = pickerList[row]
+        showPicker.text = PickerModel.pickerModel.pickerList[row]
     }
     func createPickerView(tagNo : Int) {
         let pickerView = UIPickerView()
---
+++ todo/model/PickerModel.swift
@@ -0,0 +1,18 @@
+//
+//  PickerModel.swift
+//  todo
+//
+//  Created by t2023-m0081 on 2023/09/01.
+//
+
+import Foundation
+
+struct PickerModel {
+    
+    static var pickerModel = PickerModel()
+    
+    var pickerList : [String] = ["-------" ,"혼자 할 일" , "같이 할 일"]
+    
+    init() {}
+}
+
